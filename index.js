81be42b7959a77a64babd38c1e59a5a386f2fdf8
Ola <musicola@mail.ru>
2023-03-10 14:48:44 +0300

    readme

--- index.js
+++ index.js
@@ -1,62 +1,25 @@
 'use strict';
 
-const arr = [
-    {
-      name: 'Стивен Спилберг',
-      career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
-      films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
-      top_rated_film: 'Список Шиндлера'
-    },
-    {
-      name: 'Кристофер Нолан',
-      career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
-      films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
-      top_rated_film: 'Начало'
-    },
-    {
-      name: 'Мартин МакДона',
-      career: 'Сценарист, Режиссёр, Продюсер',
-      films: 'https://www.film.ru/person/martin-makdonah',
-      top_rated_film: 'Три билборда на границе Эббинга, Миссури'
-    },
-    {
-      name: 'Алексей Балабанов',
-      career: 'Режиссёр, Сценарист, Актёр, Продюсер',
-      films: 'https://www.film.ru/person/aleksey-balabanov',
-      top_rated_film: 'Брат'
-    },
-    {
-      name: 'Питер Фаррелли',
-      career: 'Продюсер, Режиссёр, Сценарист, Актёр',
-      films: 'https://www.film.ru/person/piter-farrelli',
-      top_rated_film: 'Зелёная книга'
-    },
-    {
-      name: 'Юрий Быков',
-      career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
-      films: 'https://www.film.ru/person/yuriy-bykov',
-      top_rated_film: 'Дурак'
-    },
-    {
-      name: 'Жан-Марк Валле',
-      career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
-      films: 'https://www.film.ru/person/zhan-mark-valle',
-      top_rated_film: 'Далласский клуб покупателей'
-    },
-  ];
 
-
+//   const directorsName = document.querySelectorAll('.name');
+//   const directorsCareer = document.querySelectorAll('.career');
+//   const directorsfilm = document.querySelectorAll('.film');
 
   arr.forEach(function(item,i){
 
-    const directors = document.querySelector('.directors');
+    // directorsName[i].innerHTML =`${(i+1)}. ${arr[i].name}`;
+    // directorsCareer[i].innerHTML = arr[i].career;
+    // directorsfilm[i].href = arr[i].films;
+
+
+       const directors = document.querySelector('.directors');
 
     const directorName = document.createElement('div');
 	directorName.className = 'li';
-	directorName.innerHTML = ` <p class="name">${(i+1)}. ${arr[i]['name']}</p><br>
+	directorName.innerHTML = ` <p class="name">${(i+1)}. ${arr[i].name}</p><br>
                                 <div class="wrap">
-                                    <div class="career">${arr[i]['career']}</div>
-                                    <a href=${arr[i]['films']}>Фильмография</a>
+                                    <div class="career">${arr[i].career}</div>
+                                    <a href=${arr[i].films}>Фильмография</a>
                                 </div>`;
 	directors.append(directorName);
 
@@ -69,5 +32,31 @@ const arr = [
 
 const topFilmsList = arr.map(item => item.top_rated_film);
 document.querySelector('.best_films').textContent = topFilmsList.join(', ');
+
+
+///Задача со ***
+
+
+const arr1 = [1, 2];
+const arr2 = [2, 3];
+const arr3 = ['a', 'b'];
+const arr4 = ['b', 'c'];
+const arr5 = ['b', 'e', 'c'];
+const arr6 = ['b', 'b', 'e'];
+const arr7 = ['b', 'c', 'e'];
+const arr8 = ['b', 'e', 'c'];
+
+
+const intersection = (arrays) => {
+    // Напишите здесь своё решение
+
+
+
+   }
+   
+   
+console.log(intersection(arr1, arr2)) // [2]
+console.log(intersection(arr3, arr4, arr5)) // ['b']
+console.log(intersection(arr6, arr7, arr8)) // ['b', 'e']
 
 
